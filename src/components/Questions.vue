cac147ed7a33ce76f7542f69f13ada810dc3d967
Imran189 <imranimu189@gmail.com>
2022-10-09 22:33:39 +0600

    final commit

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -2,7 +2,7 @@
     <div class="questions-ctr">
         <div class="progress">
             <div class="bar" :style="{width:`${(questionsAnswered / questions.length)*100}%`}"></div>
-            <div class="status">{{questionsAnswered}} out f {{questions.length}} questions answered</div>
+            <div class="status">{{questionsAnswered}} out of {{questions.length}} questions answered</div>
         </div>
         <transition-group name="fade">
             <div class="single-question" v-for="(question, qi) in questions"
